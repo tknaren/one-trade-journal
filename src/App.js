1deda12aeb5cb9537af5ee7ffa428b889e892597
Naren <tknaren@gmail.com>
2023-04-28 21:28:37 +0100

    weekly scan

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Component }  from 'react';
-import Layout from './components/layout/layout';
+import React, { Component } from "react";
+import Layout from "./components/layout/layout";
 
 //theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
-    
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
 //core
 import "primereact/resources/primereact.min.css";
 
 //icons
-import "primeicons/primeicons.css";                                         
-        
+import "primeicons/primeicons.css";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Layout></Layout>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Layout></Layout>
+		</div>
+	);
 }
 
 export default App;
--- src/components/weekly-scan/weekly-scan.jsx
+++ src/components/weekly-scan/weekly-scan.jsx
@@ -1,12 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect, useMemo } from "react";
+import { Fragment } from "react";
+import { AgGridReact } from "ag-grid-react";
+import { getWeeklyScanResults } from "./../../services/weeklyscanService";
 
 class WeeklyScan extends Component {
-    state = {  } 
-    render() { 
-        return (
-                <p>Weekly Scan</p>
-        );
-    }
+	state = {
+		rowData: getWeeklyScanResults(),
+		columndefs: [
+			{ field: "date" },
+			{ field: "type" },
+			{ field: "instrument" },
+			{ field: "slPips" },
+			{ field: "tpPips" },
+			{ field: "stake" },
+			{ field: "maxPips" },
+			{ field: "pnl" },
+			{ field: "netPnL" },
+			{ field: "runningBal" },
+		],
+		defaultColDef: { sortable: true, filter: true },
+	};
+	render() {
+		return (
+			<div className="ag-theme-alpine" style={{ height: 500 }}>
+				<AgGridReact
+					rowData={this.state.rowData}
+					columnDefs={this.state.columndefs}
+					defaultColDef={this.state.defaultColDef}
+				/>
+			</div>
+		);
+	}
 }
- 
+
 export default WeeklyScan;
+/*
+function WeeklyScan() {
+	const [rowData, setRowData] = useState([]);
+
+	const [columndefs, setColDef] = useState([
+		{ field: "date" },
+		{ field: "type" },
+		{ field: "instrument" },
+		{ field: "slPips" },
+		{ field: "tpPips" },
+		{ field: "stake" },
+		{ field: "maxPips" },
+		{ field: "pnl" },
+		{ field: "netPnl" },
+		{ field: "runningBal" },
+	]);
+
+	useEffect(() => {
+		fetch("../services/Snippers.json")
+			.then((result) => result.json())
+			.then((rowData) => setRowData(rowData))
+			.catch((error) => console.error(error));
+	}, []);
+
+	console.log("props", rowData);
+
+	return (
+		<div className="ag-theme-alpine" style={{ height: 500 }}>
+			<AgGridReact rowData={rowData} columnDefs={columndefs} />
+		</div>
+	);
+}
+
+export default WeeklyScan;
+*/
