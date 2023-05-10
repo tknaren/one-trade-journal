4eae4ef4f141119c0a1e453257bd76ca1a004983
Naren Krishnamurthy <50633755+tknaren@users.noreply.github.com>
2023-05-10 21:12:05 +0100

    Create config.js

---
+++ src/components/config.js
@@ -0,0 +1,6 @@
+export const alphaVant = {
+    api_token: '5PU4ARA6NIXLUP00',
+    base_url: 'https://www.alphavantage.co/query'
+    
+}
+// https://www.alphavantage.co/query?function=EMA&symbol=USDEUR&interval=daily&time_period=13&series_type=close&apikey=5PU4ARA6NIXLUP00
