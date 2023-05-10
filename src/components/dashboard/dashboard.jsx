e6a70e8cbe91edf355f9e63b963b14935cd355d2
Naren Krishnamurthy <50633755+tknaren@users.noreply.github.com>
2023-05-10 21:09:58 +0100

    Update dashboard.jsx

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,50 +1,27 @@
 import React, { Component } from 'react';
-import { Button } from 'primereact/button';   
+import { Button } from 'primereact/button';  
+import StockRow from '../stock/stock-row'; 
 
-class Dashboard extends Component {
-    state = { 
-        count: 0,
-        tags: ['one','two','three']
-     } 
-
-    // constructor(){
-    //     super();
-    //     console.log('Constructor', this);
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
-
-    render() { 
-        return (
-            <div>
-                <p>Dashboard</p>
-                <span className='badge badge-primary m-2'>{ this.formatCount() }</span>
-                {/* Simply pass the reference of the method to the click event  */}
-                <button onClick = {this.handleIncrement} className='btn btn-primary'>Increment</button>
-                <div>
-                    { this.renderTags() }
-                </div>
-
-                <Button label="Check" size="sm" />
-            </div>    
-        );
-    }
-
-    handleIncrement = () => {
-        console.log('please log ', this);
-    }
-    
-    renderTags(){
-        if (this.state.count === 0) return <p>No Tags!</p>
-
-        return <ul>{ this.state.tags.map (tag => <li key={tag}> { tag } </li>)}</ul>
-    }
-
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
-    }
-
+function Dashboard() {
+    return (
+        <div className='Dashboard'>
+            <div className='container'>
+                <table className='table mt-5'>
+                    <thead>
+                        <tr>
+                            <th>Ticker</th>
+                            <th>Price</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <StockRow ticker="USDEUR" />
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
- 
 
 export default Dashboard;
